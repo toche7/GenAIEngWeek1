ac6c3582c73eb67273ea78c34058a5b18d11b605
Ake <t.samanchuen@gmail.com>
2024-09-13 12:59:26 +0700

    add more models in lab3 and lab4

--- Lab1basic.py
+++ Lab1basic.py
@@ -1,48 +1,13 @@
 
 # what should i demo here?
 # 1. print statement
-print('hello World')
+ 
 # 2. variable declaration
-a = 10
-b = 20
-c = a + b
-print(c)
-
 # 3. if else statement
-if a > b:
-    print('a is greater')
-else:
-    print('b is greater')
-
 # 4. for loop
-for i in range(10):
-    print(i)
-
 # 5. while loop
-i = 0
-while i < 10:
-    print(i)
-    i += 1
-
 # 6. function
-def add(a, b):
-    return a + b
-
-print(add(10, 20))
-
-# 7. class
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def display(self):
-        print(self.name)
-        print(self.age)
-    
-p = Person('John', 30)
-p.display()
-
+ 
 
 
 
